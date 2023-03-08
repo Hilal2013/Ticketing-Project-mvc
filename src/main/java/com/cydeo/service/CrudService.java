03b305b0e503717714a1f37e9bfad202a09bc0a9
Hilal2013 <hilalozkul@gmail.com>
2023-03-08 12:09:47 -0500

    CrudService

---
+++ src/main/java/com/cydeo/service/CrudService.java
@@ -0,0 +1,17 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.UserDTO;
+
+import java.util.List;
+
+public interface CrudService <T,ID> {
+
+    //    UserDTO save(UserDTO user);
+//    UserDTO findById(String username);
+//    List<UserDTO> findAll();
+//    void deleteById(String userName);
+    T save(T user);
+    T findById(ID username);
+    List<T> findAll();
+    void deleteById(ID userName);
+}
