1c03368f9cc4007682d3d8e0145d75ede01b7c57
Hilal2013 <hilalozkul@gmail.com>
2023-03-09 19:01:54 -0500

    updated

--- src/main/java/com/cydeo/converter/RoleDtoConverter.java
+++ src/main/java/com/cydeo/converter/RoleDtoConverter.java
@@ -1,24 +1,23 @@
 package com.cydeo.converter;
-
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.service.RoleService;
+
 import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationPropertiesBinding
-public class RoleDTOConverter implements Converter<String,RoleDTO> {
-
-    public RoleDTOConverter(RoleService roleService) {
-        this.roleService = roleService;
-    }
+public class RoleDtoConverter implements Converter<String, RoleDTO> {
 
     RoleService roleService;
 
+    public RoleDtoConverter(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
     @Override
-    public RoleDTO convert(String source) {//source is String
-        //go to database find by id
-        return roleService.findById(Long.parseLong(source));//convert to long
+    public RoleDTO convert(String source) {
+        return roleService.findById(Long.parseLong(source));
     }
 }
