05210595c157aa22a180507d427b6f8f9e0b9080
Hilal2013 <hilalozkul@gmail.com>
2023-03-14 10:39:55 -0400

    updated

--- src/main/java/com/cydeo/service/ProjectService.java
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -4,4 +4,6 @@ import com.cydeo.dto.ProjectDTO;
 
 public interface ProjectService extends  CrudService<ProjectDTO,String>{
 //first one which object->ProjectDTO (value);  second one String->projectcode unique item(key))
+
+    void complete(ProjectDTO project);
 }
