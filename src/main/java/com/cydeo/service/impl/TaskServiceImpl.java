a4c9a16a97bd53ffcc878a87339c7c81425fc6ad
Hilal2013 <hilalozkul@gmail.com>
2023-03-15 15:13:10 -0400

    updated

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -76,7 +76,13 @@ public class TaskServiceImpl extends AbstractMapService<TaskDTO,Long> implements
     @Override
     public List<TaskDTO> findAllTasksByStatusIsNot(Status status) {
         return findAll().stream()
-                .filter(task->task.getTaskStatus().equals(status))
+                .filter(task->!task.getTaskStatus().equals(status))
                 .collect(Collectors.toList());
     }
+
+    @Override
+    public List<TaskDTO> findAllTasksByStatus(Status status) {
+        return findAll().stream()
+                .filter(task->task.getTaskStatus().equals(status))
+                .collect(Collectors.toList());    }
 }
