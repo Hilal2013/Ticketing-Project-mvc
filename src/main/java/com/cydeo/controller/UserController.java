7af912911e143a8d5718c23e8a8ecb2c440155a8
Hilal2013 <hilalozkul@gmail.com>
2023-03-15 19:59:25 -0400

    updated

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -5,7 +5,10 @@ import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
@@ -25,11 +28,18 @@ public class UserController {
         model.addAttribute("roles", roleService.findAll());
         model.addAttribute("users", userService.findAll());
 
-        return "/user/create";
+        return "user/create";
     }
 
     @PostMapping("/create")
-    public String insertUser(@ModelAttribute("user") UserDTO user) {
+    public String insertUser(@Valid @ModelAttribute("user") UserDTO user, BindingResult bindingResult,Model model) {
+//if there is error
+if(bindingResult.hasErrors()){
+    model.addAttribute("roles", roleService.findAll());
+    model.addAttribute("users", userService.findAll());
+
+    return "user/create";//file//dont redirect
+}
 
         userService.save(user);
         return "redirect:/user/create";
@@ -49,7 +59,7 @@ public class UserController {
         return "user/update";
     }
     @PostMapping("/update")
-    public String updateUser(@ModelAttribute("user") UserDTO user){
+    public String updateUser(@Valid @ModelAttribute("user") UserDTO user){
 //i need to update that user
 userService.update(user);
 return "redirect:/user/create";
