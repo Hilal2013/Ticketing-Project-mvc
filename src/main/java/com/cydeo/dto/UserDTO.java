1f0c4236f57476c98463b9175d2222bd54cd5ac5
Hilal2013 <hilalozkul@gmail.com>
2023-03-15 19:09:04 -0400

    added limitations

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -6,17 +6,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
+    @NotBlank
+    @Size(max=15,min=12)
     private String firstName;
+    @NotBlank
+    @Size(max=15,min=12)
     private String lastName;
+    @NotBlank
+    @Email //it will be email format
     private String userName;
+    @NotBlank
+    @Pattern (regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
     private String passWord;
+
     private boolean enabled;
+
+    @NotBlank
+    @Pattern(regexp = "^\\d{10}$")//should have 10 characters and only numbers
     private String phone;
+@NotNull
     private RoleDTO role;
+@NotNull
     private Gender gender;
 
 }
