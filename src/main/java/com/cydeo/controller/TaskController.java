cba9c201077ccd7f940b5c1131e758e3a6591811
Hilal2013 <hilalozkul@gmail.com>
2023-03-14 23:54:30 -0400

    updated

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -6,9 +6,8 @@ import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/task")
@@ -23,8 +22,9 @@ public class TaskController {
         this.projectService = projectService;
         this.taskService = taskService;
     }
+
     @GetMapping("/create")
-    public String createTask(Model model){
+    public String createTask(Model model) {
         model.addAttribute("task", new TaskDTO());
         model.addAttribute("projects", projectService.findAll());
         model.addAttribute("employees", userService.findEmployees());
@@ -33,11 +33,55 @@ public class TaskController {
     }
 
     @PostMapping("/create")
-    private String insertTask(TaskDTO task){
+    private String insertTask(TaskDTO task) {
         taskService.save(task);
 
         return "redirect:/task/create";
     }
+
+    @GetMapping("/delete/{id}")
+    private String deleteTask(@PathVariable("id") Long id) {
+        taskService.deleteById(id);
+        return "redirect:/task/create";
+    }
+
+    @GetMapping("/update/{taskId}")
+    public String editTask(@PathVariable("taskId") Long taskId, Model model) {
+
+        model.addAttribute("task", taskService.findById(taskId));
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("employees", userService.findEmployees());
+        model.addAttribute("tasks", taskService.findAll());
+
+        return "/task/update";
+
+    }
+
+
+
+/*
+    @PostMapping("/update/{taskId}")
+    public String updateTask(@PathVariable("taskId") Long taskId, TaskDTO task) {
+
+        task.setId(taskId);
+        taskService.update(task);
+
+        return "redirect:/task/create";
+
+    }
+
+ */
+
+    @PostMapping("/update/{id}")
+    public String updateTask(TaskDTO task) {
+        taskService.update(task);
+
+        return "redirect:/task/create";
+
+    }
+
+
+
 
 
 }
